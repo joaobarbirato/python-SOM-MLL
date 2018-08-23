10e898a808943621d9110e13cd449b13dc044492
joao barbirato <joaobarbirato@gmail.com>
2018-08-22 22:13:54 -0300

    Adicionado tratamento de cardinalidade em metrics

--- metrics.py
+++ metrics.py
@@ -1,23 +1,41 @@
+#!venv/bin/python
 def precision(y_vdd,y_prev):
     S = len(y_prev)
     T = len(y_prev[0])
     soma = 0
     for i in range(S):
-        len(y_vdd[i])
-        for j in range(len(y_vdd[i])):
-            if y_vdd[i][j] == y_prev[i][j]:
-                soma += 1
-    return soma/(S*T)
+        card_intersec = 0
+        for j in range(T):
+            if y_vdd[i][j] == y_prev[i][j] and y_vdd[i][j] == 1: # conta se sao iguais
+                card_intersec += 1
+
+        card_classes_prev = 0
+        for j in range(T):
+            if y_prev[i][j] == 1:
+                card_classes_prev += 1
+
+        soma += card_intersec / card_classes_prev
+            
+    return soma/S
 
 def recall(y_vdd,y_prev):
-    S = len(y_vdd)
+    S = len(y_prev)
     T = len(y_prev[0])
     soma = 0
     for i in range(S):
-        for j in range(len(y_vdd[i])):
-            if y_vdd[i][j] == y_prev[i][j]:
-                soma += 1
-    return soma/(S*T)
+        card_intersec = 0
+        for j in range(T):
+            if y_vdd[i][j] == y_prev[i][j] and y_vdd[i][j] == 1: # conta se sao iguais
+                card_intersec += 1
+
+        card_classes_vdd = 0
+        for j in range(T):
+            if y_vdd[i][j] == 1:
+                card_classes_vdd += 1
+
+        soma += card_intersec / card_classes_vdd
+            
+    return soma/S
 
 def medida_f(y_vdd,y_prev):
     p = precision(y_vdd, y_prev)
