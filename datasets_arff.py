f9ef9ce0536f8af64932ea731d506d86e0085404
joao barbirato <joaobarbirato@gmail.com>
2018-08-24 23:38:20 -0300

    Criado modulo para datasets arff

---
+++ datasets_arff.py
@@ -0,0 +1,47 @@
+import csv
+import arff
+import numpy as np
+from time import time
+from metrics import *
+from time import clock
+from kohonen import Mapa
+from datetime import date
+from kfold import train_test_kfold
+from sklearn.preprocessing import scale
+
+DATASET_NUMEROCLASSES = (
+    ('CAL500', 174),
+    ('emotions', 6),
+    ('mediamill', 101)
+)
+
+for dataset, n_classes in DATASET_NUMEROCLASSES:
+    data = arff.load(open('datasets/'+dataset+'.arff','r'))
+    exemplos = data['data']                             # dataset
+    n_atributos = len(data['attributes']) - n_classes   # n. de atributos
+    n_exemplos = len(exemplos)                          # n. de exemplos
+    taxa = .1                                           # taxa de aprendizado
+    decaimento = 10000
+    lista_n_dimensoes = [ 10,  20,  25]
+    x = scale(np.ndarray([data['data'][i][:-n_classes] for i in range(len(n_exemplos))]))
+    y = np.ndarray([data['data'][i][-n_classes:] for i in range(len(n_exemplos))])
+
+    with open('resultados/' + dataset + '/' + str(date.today()) + str(time()) + dataset + '_resultado.csv', 'w') as tabela:
+        writer = csv.DictWriter(tabela, fieldnames=['usa_vizinhos','n_dimensoes','precisao','cobertura','medida_f'])
+        writer.writeheader()
+        t0 = clock()
+        for bool_val in [False, True]:
+            for n_dimensoes in lista_n_dimensoes:
+                som = Mapa(taxa=taxa,dimensao=n_dimensoes,fi0=8,features=n_atributos,decaimento=decaimento, nclasses=n_classes)
+                [y_v,y_p] = train_test_kfold(10,True,None,som,x,y,vizinhos=bool_val)
+                row = {
+                    'usa_vizinhos': bool_val,
+                    'n_dimensoes': n_dimensoes,
+                    'precisao': precision(y_v, y_p),
+                    'cobertura': recall(y_v, y_p),
+                    'medida_f': medida_f(y_v, y_p),
+                }
+                writer.writerow(row)
+                print(row)
+
+    print("CSV de " + dataset + " pronto! Apos "+ str(clock()-t0) + "Segundos!")
